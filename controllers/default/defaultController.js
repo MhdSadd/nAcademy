8c72435d99c168207874330152b218b82638dd07
Xavier-Platinum <kwislawrencekwis@gmail.com>
2020-11-21 11:29:27 +0100

    changes

--- controllers/default/defaultController.js
+++ controllers/default/defaultController.js
@@ -2,5 +2,17 @@ module.exports = {
     index: (req, res) => {
         const pagetitle = "Home";
         res.render("defaultViews/index", {pagetitle});
+    },
+    about: (req, res) => {
+        const pagetitle = "About";
+        res.render("defaultViews/about", {pagetitle});
+    },
+    contact: (req, res) => {
+        const pagetitle = "Contact";
+        res.render("defaultViews/contact", {pagetitle});
+    },
+    course: (req, res) => {
+        const pagetitle = "Courses";
+        res.render("defaultViews/course", {pagetitle});
     }
 }
--- routes/default/defaultRoutes.js
+++ routes/default/defaultRoutes.js
@@ -3,9 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 // destructuring controllers
-const { index } = require("../../controllers/default/defaultController")
+const { index, about, contact, course } = require("../../controllers/default/defaultController")
 
 // home route
-router.get("/", index)
+router.get("/", index);
+
+// about route
+router.get("/about", about);
+
+// contact route
+router.get("/contact", contact);
+
+// courses route 
+router.get("/course", course);
 
 module.exports = router;
